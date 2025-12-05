469d296c9e5b0278f534cd1b1c5e7fa6149db131
kimgho <kgh67811@knu.ac.kr>
2025-12-05 19:44:29 +0900

    feat(package): tanstack query, sprinkles 패키지 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import tseslint from "typescript-eslint";
 import next from "@next/eslint-plugin-next";
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
 const eslintConfig = [
   ...tseslint.configs.recommended,
@@ -9,6 +10,7 @@ const eslintConfig = [
     plugins: {
       "@next/next": next,
       "jsx-a11y": jsxA11y,
+      "@tanstack/query":pluginQuery,
     },
     languageOptions: {
       parser: tseslint.parser,
@@ -24,6 +26,13 @@ const eslintConfig = [
       ...next.configs["core-web-vitals"].rules,
       ...jsxA11y.flatConfigs.recommended.rules,
       "jsx-a11y/alt-text": "error",
+      "@tanstack/query/exhaustive-deps":"error",
+      "@tanstack/query/no-rest-destructuring":"warn",
+      "@tanstack/query/stable-query-client":"error",
+      "@tanstack/query/no-unstable-deps":"warn",
+      "@tanstack/query/infinite-query-property-order":"warn",
+      "@tanstack/query/no-void-query-fn":"error",
+      "@tanstack/query/mutation-property-order":"warn",
     },
   },
   {
