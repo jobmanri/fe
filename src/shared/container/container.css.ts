c6235297cb0e50502034b6beb052672a95830321
kimgho <kgh67811@knu.ac.kr>
2025-12-12 00:50:44 +0900

    feat(shared): 공용 컨테이너 컴포넌트 추가

---
+++ src/shared/container/container.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  display: "flex",
+  flexDirection: "column",
+  backgroundColor: "#ffffff",
+  border: "1px solid rgba(0, 0, 0, 0.10)",
+  borderRadius: "24px",
+  padding: "40px",
+});
