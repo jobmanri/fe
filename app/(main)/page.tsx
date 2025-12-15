e4a98e8578b39719be57379fd5e797e90d57bf46
kimgho <kgh67811@knu.ac.kr>
2025-12-15 23:54:46 +0900

    feat(page): feed section 추가

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,14 @@
+import { Spacing } from "@/shared/ui/spacing";
+
 import { CompanyListSection } from "./_components/CompanyListSection";
+import { FeedSection } from "./_components/FeedSection";
 
 export default function Home() {
-  return <CompanyListSection />;
+  return (
+    <>
+      <CompanyListSection />
+      <Spacing direction="vertical" size={30} />
+      <FeedSection />
+    </>
+  );
 }
