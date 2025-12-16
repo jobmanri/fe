47f58655a8d638b904c00574e3521c8343e4a86e
kimgho <kgh67811@knu.ac.kr>
2025-12-16 16:19:21 +0900

    fix(feed): h3태그로 변경

--- src/entities/feed/ui/FeedHeader.tsx
+++ src/entities/feed/ui/FeedHeader.tsx
@@ -9,7 +9,7 @@ export const FeedHeader = () => {
         <Badge>태그</Badge>
         <span className={styles.tagText}>피드 분류</span>
       </header>
-      <h1 className={styles.cardTitle}>피드 제목</h1>
+      <h3 className={styles.cardTitle}>피드 제목</h3>
       <div className={styles.cardMeta}>
         <span>작성자</span>
         <span>날짜</span>
